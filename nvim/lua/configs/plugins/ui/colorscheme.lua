bb49714132f0ae7c6e107e728df80d48463b1a5b
Yu-Hao Chang <9203700@gmail.com>
2025-06-06 13:32:10 +0800

    Add gruvbox colorscheme plugin that supports treesitter highlight.

--- nvim/lua/configs/plugins/ui/colorscheme.lua
+++ nvim/lua/configs/plugins/ui/colorscheme.lua
@@ -1,3 +1,31 @@
 local utils = require('configs.utils')
+local colorscheme_plugin_spec = nil
 
-return utils.construct_plugin_default_spec('morhetz/gruvbox')
+if vim.fn.has('nvim-0.8.0') == 1 then
+  colorscheme_plugin_spec = utils.construct_plugin_default_spec(
+    'ellisonleao/gruvbox.nvim',
+    {
+      priority = 1000,
+      config = true,
+      opts = {
+        -- Disable italic in highlight.
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+      },
+    }
+  )
+else
+  colorscheme_plugin_spec = utils.construct_plugin_default_spec(
+    'morhetz/gruvbox',
+    {
+      priority = 1000,
+    }
+  )
+end
+
+return colorscheme_plugin_spec
