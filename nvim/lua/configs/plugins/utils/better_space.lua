52c0af3efab114c1c3598d27af0e2b39b2ce0926
Yu-Hao Chang <9203700@gmail.com>
2025-06-09 11:12:35 +0800

    Override default highlight disabled filetypes for vim-better-whitespace plugin.

--- nvim/lua/configs/plugins/utils/better_space.lua
+++ nvim/lua/configs/plugins/utils/better_space.lua
@@ -6,6 +6,17 @@ return utils.construct_plugin_default_spec(
     init = function()
       -- Disable auto stripping trailing spaces on file saving.
       vim.g['strip_whitespace_on_save'] = 0
+
+      -- Override default highlight disabled file types.
+      vim.g['better_whitespace_filetypes_blacklist'] = {
+        'diff',
+        'git',
+        'gitcommit',
+        'unite',
+        'qf',
+        'help',
+        'fugitive'
+      }
     end
   }
 )
