c950ca06c72f39c96812ad66f67da9746c557008
Yu-Hao Chang <9203700@gmail.com>
2026-06-13 16:24:41 +0800

    Adjust nvim-tree configurations.
    1. Make sure older neovim use correct version tag.
    2. Adjust git signs.
    3. Adjust related key-mapping configurations.

--- nvim/lua/configs/plugins/utils/file_explorer.lua
+++ nvim/lua/configs/plugins/utils/file_explorer.lua
@@ -1,7 +1,10 @@
-local file_explorer_plugin_spec = nil
+local string = require('string')
 local utils = require('configs.utils')
 
-if vim.fn.has('nvim-0.9.0') == 1 then
+local file_explorer_plugin_spec = nil
+
+if vim.fn.has('nvim-0.6.0') == 1 then
+  local stable_minimum_supported_nvim_version = 'nvim-0.10.0'
   local function wrap_function(func, ...)
     -- Reference: https://www.reddit.com/r/neovim/comments/sjiwox/question_give_arguments_to_vimkeymapsets_function/
     local args = {...}
@@ -10,135 +13,158 @@ if vim.fn.has('nvim-0.9.0') == 1 then
     end
   end
 
-  file_explorer_plugin_spec = utils.construct_plugin_default_spec(
-    'nvim-tree/nvim-tree.lua',
-    {
-      version = '*',
-      lazy = false,
-      opts = {
-        disable_netrw = true,
-        hijack_netrw = true,
-        sort_by = 'case_sensitive',
-        modified = {
-          enable = true,
-          show_on_dirs = true,
-          show_on_open_dirs = true,
+  local spec_table = {
+    lazy = false,
+    opts = {
+      disable_netrw = true,
+      hijack_netrw = true,
+      sort_by = 'case_sensitive',
+      modified = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = true,
+      },
+      filters = {
+        custom = {
+          [[\.pyc$]],
+          '__pycache__',
         },
-        filters = {
-          custom = {
-            [[\.pyc$]],
-            '__pycache__',
+        exclude = {},
+      },
+      renderer = {
+        group_empty = true,
+        add_trailing = true,
+        icons = {
+          git_placement = 'before',
+          modified_placement = 'before',
+          webdev_colors = false,
+          symlink_arrow = ' -> ',
+          show = {
+            file = false,
+            folder = false,
+            folder_arrow = true,
+            git = true,
+            modified = true,
           },
-          exclude = {},
-        },
-        renderer = {
-          group_empty = true,
-          add_trailing = true,
-          icons = {
-            git_placement = 'before',
-            modified_placement = 'before',
-            webdev_colors = false,
-            symlink_arrow = ' -> ',
-            show = {
-              file = false,
-              folder = false,
-              folder_arrow = true,
-              git = true,
-              modified = true,
+          glyphs = {
+            folder = {
+              arrow_closed = '▸',
+              arrow_open = '▾',
             },
-            glyphs = {
-              folder = {
-                arrow_closed = '▸',
-                arrow_open = '▾',
-              },
-              symlink = '[L]',
-              modified = '[+]',
-              git = {
-                unstaged = "X",
-                staged = "O",
-                unmerged = "^",
-                renamed = "[GR]",
-                untracked = "?",
-                deleted = "[GD]",
-                ignored = "[GI]",
-              },
+            symlink = '[L]',
+            modified = '[+]',
+            git = {
+              unstaged = "[X]",
+              staged = "[O]",
+              unmerged = "[^]",
+              renamed = "[R]",
+              untracked = "[?]",
+              deleted = "[D]",
+              ignored = "[I]",
             },
           },
         },
       },
-      config = function(_, opts)
-        local nvimtree = require('nvim-tree')
-        local api = require("nvim-tree.api")
+    },
+    config = function(_, opts)
+      local nvimtree = require('nvim-tree')
+      local api = require("nvim-tree.api")
 
-        vim.keymap.set('n', '<F6>', api.tree.toggle)
-
-        function set_custom_key_binding(buffer_number)
-          local function ops_nowait(desc)
-            return {
-              desc = "nvim-tree: " .. desc,
-              buffer = buffer_number,
-              noremap = true,
-              silent = true,
-              nowait = true,
-            }
-          end
-
-          local function ops_wait(desc)
-            return {
-              desc = "nvim-tree: " .. desc,
-              buffer = buffer_number,
-              noremap = true,
-              silent = true,
-              nowait = false,
-            }
-          end
-
-          -- Apply default mappings
-          api.config.mappings.default_on_attach(buffer_number)
-
-          -- Custom mappings
-          vim.keymap.set(
-            'n',
-            '<C-t>',
-            api.tree.change_root_to_parent,
-            ops_nowait('Up')
-          )
-          vim.keymap.set(
-            'n',
-            '?',
-            api.tree.toggle_help,
-            ops_nowait('Help')
-          )
-          vim.keymap.set(
-            'n',
-            'i',
-            api.node.open.horizontal,
-            ops_nowait('Open Horizontal split')
-          )
-          vim.keymap.set(
-            'n',
-            '<C-]>',
-            api.tree.change_root_to_node,
-            ops_nowait('Change root to current directory')
-          )
-          vim.keymap.set(
-            'n',
-            't',
-            api.node.open.tab,
-            ops_wait('Open in new tab')
-          )
-          vim.keymap.set(
-            'n',
-            'T',
-            wrap_function(api.node.open.tab, nil, {focus=false}),
-            ops_nowait('Open in new tab silently')
-          )
+      function set_custom_key_binding(buffer_number)
+        local function ops_nowait(desc)
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = buffer_number,
+            noremap = true,
+            silent = true,
+            nowait = true,
+          }
         end
 
-        opts.on_attach = set_custom_key_binding
-        nvimtree.setup(opts)
-      end,
-    }
+        local function ops_wait(desc)
+          return {
+            desc = "nvim-tree: " .. desc,
+            buffer = buffer_number,
+            noremap = true,
+            silent = true,
+            nowait = false,
+          }
+        end
+
+        -- Apply default mappings
+        if api.config.map then
+          api.config.map.on_attach.default(buffer_number)
+        else
+          api.config.mappings.default_on_attach(buffer_number)
+        end
+
+        -- Custom mappings
+        vim.keymap.set(
+          'n',
+          '<C-t>',
+          api.tree.change_root_to_parent,
+          ops_nowait('Up')
+        )
+        vim.keymap.set(
+          'n',
+          '?',
+          api.tree.toggle_help,
+          ops_nowait('Help')
+        )
+        vim.keymap.set(
+          'n',
+          'i',
+          api.node.open.horizontal,
+          ops_nowait('Open Horizontal split')
+        )
+        vim.keymap.set(
+          'n',
+          '<C-]>',
+          api.tree.change_root_to_node,
+          ops_nowait('Change root to current directory')
+        )
+        vim.keymap.set(
+          'n',
+          't',
+          api.node.open.tab,
+          ops_wait('Open in new tab')
+        )
+        vim.keymap.set(
+          'n',
+          'T',
+          wrap_function(api.node.open.tab, nil, {focus=false}),
+          ops_nowait('Open in new tab silently')
+        )
+      end
+
+      opts.on_attach = set_custom_key_binding
+      nvimtree.setup(opts)
+
+      vim.keymap.set(
+        'n',
+        '<F6>',
+        wrap_function(api.tree.toggle, {find_file=true})
+      )
+      vim.keymap.set(
+        'n',
+        '<leader>f',
+        wrap_function(api.tree.find_file, {open = true, focus=true})
+      )
+    end,
+  }
+
+  if 0 == vim.fn.has(stable_minimum_supported_nvim_version) then
+    -- For older neovim, use corresponding compatible tag.
+    spec_table['tag'] = string.format(
+      'compat-nvim-%d.%d',
+      vim.version().major,
+      vim.version().minor
+    )
+  end
+
+  file_explorer_plugin_spec = utils.construct_plugin_default_spec(
+    'nvim-tree/nvim-tree.lua',
+    spec_table
   )
 else
   local keymap = vim.keymap
